378c3a6932a3a56f4559a896c7cdad24a84a0363
lybsoon <kacper.breczko@wp.pl>
2025-02-27 20:43:51 +0100

    1 zadanie

---
+++ Classes1/exercise2.py
@@ -0,0 +1,4 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print(2*x+5*y)
---
+++ Classes1/exercise3.py
@@ -0,0 +1,6 @@
+a = str(input("Enter your name: "))
+b = str(input("Enter your last name: "))
+c = int(input("Enter your age: "))
+d = str(input("Specify your field of study: "))
+
+print("Jestem", a,b ,"mam", c, "lat studiuję", d)
